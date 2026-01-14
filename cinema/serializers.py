4cacd38fb11f88ed0db49eb986788cc43c388c2b
TimoteoRJanzen <timoteojanzen123@gmail.com>
2026-01-14 15:19:24 -0300

    feat: add serializers for Order and Ticket

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -5,7 +5,7 @@ from cinema.models import (
     Genre,
     Actor,
     Movie,
-    MovieSession,
+    MovieSession, Ticket, Order,
 )
 
 
@@ -44,3 +44,25 @@ class MovieSessionSerializer(serializers.ModelSerializer):
     class Meta:
         model = MovieSession
         fields = ["id", "show_time", "movie", "cinema_hall"]
+
+
+class TicketSerializer(serializers.ModelSerializer):
+    def validate(self, attrs):
+        Ticket.validate_seat(
+            row=attrs["row"],
+            seat=attrs["seat"],
+            cinema_hall=attrs["movie_session"].cinema_hall
+        )
+        return attrs
+
+    class Meta:
+        model = Ticket
+        fields = ("id", "row", "seat", "movie_session")
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    tickets = TicketSerializer(many=True, read_only=False, allow_empty=False)
+
+    class Meta:
+        model = Order
+        fields = ("id", "tickets", "created_at")
