73e32308bbebb44447261ed9c4281445d9b27df2
TimoteoRJanzen <timoteojanzen123@gmail.com>
2026-01-14 15:18:59 -0300

    feat: register Order and Ticket models in admin

--- cinema/admin.py
+++ cinema/admin.py
@@ -5,11 +5,26 @@ from cinema.models import (
     Genre,
     Actor,
     Movie,
-    MovieSession
+    MovieSession,
+    Order,
+    Ticket,
 )
+
+
+class TicketInLine(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [TicketInLine]
+
+
 
 admin.site.register(CinemaHall)
 admin.site.register(Genre)
 admin.site.register(Actor)
 admin.site.register(Movie)
 admin.site.register(MovieSession)
+admin.site.register(Ticket)
+admin.site.register(Order, OrderAdmin)
