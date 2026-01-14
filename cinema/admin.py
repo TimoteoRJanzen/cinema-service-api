5aa57751a9de6ce767e4064f1bd65c8d4fe4331f
TimoteoRJanzen <timoteojanzen123@gmail.com>
2026-01-14 10:20:33 -0300

    chore: register models on the admin page

--- cinema/admin.py
+++ cinema/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from cinema.models import (
+    CinemaHall,
+    Genre,
+    Actor,
+    Movie,
+    MovieSession
+)
+
+admin.site.register(CinemaHall)
+admin.site.register(Genre)
+admin.site.register(Actor)
+admin.site.register(Movie)
+admin.site.register(MovieSession)
