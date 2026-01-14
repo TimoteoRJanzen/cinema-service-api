5172729bcb2793914fd369190808213dcb4cb3bd
TimoteoRJanzen <timoteojanzen123@gmail.com>
2026-01-14 10:07:09 -0300

    feat: add CinemaHall, Genre, Actor, Movie and MovieSession models

--- cinema/models.py
+++ cinema/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
-# Create your models here.
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    @property
+    def full_name(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    duration = models.IntegerField()
+    genres = models.ManyToManyField(Genre, related_name="movies")
+    actors = models.ManyToManyField(Actor, related_name="movies")
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class MovieSession(models.Model):
+    show_time = models.DateTimeField()
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="movie_sessions")
+    cinema_hall = models.ForeignKey(CinemaHall, on_delete=models.CASCADE, related_name="movie_sessions")
+
+    class Meta:
+        ordering = ["-show_time"]
+
+    def __str__(self) -> str:
+        return f"{self.movie.title} {self.show_time}"
